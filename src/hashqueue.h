d32bd3cff9a08bebe60ae07da5dabc68ae132afa
Guohao Dou <guohao.dou@epfl.ch>
2022-03-01 13:33:42 +0100

    added eager queue

--- src/hashqueue.h
+++ src/hashqueue.h
@@ -8,16 +8,30 @@
 
 namespace data_structures::detail {
     template<typename T>
-    struct Entry {
+    struct EntryBase {
         template<typename ...Arg>
-        explicit Entry(double time, Arg&&... args): time{time}, payload{std::forward<Arg>(args)...}, exist{true} {}
+        explicit EntryBase(double time, Arg&&... args): time{time}, payload{std::forward<Arg>(args)...} {}
         double time;
         T payload;
+    };
+
+    template<typename T>
+    struct LazyEntry: public EntryBase<T> {
+        template<typename ...Arg>
+        explicit LazyEntry(double time, Arg&&... args): EntryBase<T>(time, std::forward<Arg>(args)...), exist{true} {}
         bool exist;
     };
 
     template<typename T>
-    inline bool compare(const Entry<T>* e1, const Entry<T>* e2) {
+    struct EagerEntry: public EntryBase<T> {
+        using size_type = typename std::vector<EagerEntry<T>*>::size_type;
+        template<typename ...Arg>
+        EagerEntry(size_type loc, double time, Arg&&... args): loc{loc}, EntryBase<T>(time, std::forward<Arg>(args)...){}
+        size_type loc;  // an eager entry keeps track of its index in the vector
+    };
+
+    template<typename T>
+    inline bool compare(const EntryBase<T>* e1, const EntryBase<T>* e2) {
         // true if e1 less than e2 => turn into a min-heap
         return e1->time > e2->time;
     }
@@ -25,14 +39,10 @@ namespace data_structures::detail {
 
 namespace data_structures {
     template<typename T, typename ID=T, typename Hash=std::hash<ID>, typename KeyEqual=std::equal_to<ID>>
-    class HashQueue {
-    private:
+    class QueueBase {
+    protected:
         std::optional<std::function<ID(const T& payload)>> id_func;
-        std::vector<detail::Entry<T>*> _data;
-        using size_type = typename decltype(_data)::size_type;
-        size_type _size{};
-        std::unordered_map<ID, detail::Entry<T>*, Hash, KeyEqual> _m;
-    private:
+
         ID convert(const T& payload) const {  // resolve ID converter
             if (id_func) {
                 return id_func.value()(payload);
@@ -47,43 +57,185 @@ namespace data_structures {
                 }
             }
         }
-    public:
-        HashQueue() = default;
+
+        QueueBase() = default;
 
         template<typename F, std::enable_if_t<std::is_constructible_v<decltype(id_func), F>, bool> = true>
-        explicit HashQueue(F f): id_func{f} {}
+        explicit QueueBase(F f): id_func{f} {}
+    };
+}
+
+namespace data_structures {
+    template<typename T, typename ID=T, typename Hash=std::hash<ID>, typename KeyEqual=std::equal_to<ID>>
+    class EagerQueue: public QueueBase<T, ID, Hash, KeyEqual> {
+    private:
+        std::vector<detail::EagerEntry<T>*> _data;
+        using size_type = typename decltype(_data)::size_type;
+        std::unordered_map<ID, detail::EagerEntry<T>*, Hash, KeyEqual> _m;
+    private:
+        size_type left(size_type i) const { return 2*i+1; }
+        size_type right(size_type i) const { return 2*i+2; }
+        size_type parent(size_type i) const { return (i-1)/2; }  // UB when i==0
+        bool perc_down(const size_type idx) {  // return true if any perc-down actually happens
+            size_type min_idx = idx;
+            const size_type left_idx = left(idx);
+            const size_type right_idx = right(idx);
+            if (left_idx < _data.size() && _data[left_idx]->time < _data[min_idx]->time) {
+                min_idx = left_idx;
+            }
+            if (right_idx < _data.size() && _data[right_idx]->time < _data[min_idx]->time) {
+                min_idx = right_idx;
+            }
+            if (min_idx == idx) {  // heap property intact on idx; no pd needed
+                return false;
+            } else {  // swap and update loc
+                std::swap(_data[idx], _data[min_idx]);
+                _data[min_idx]->loc = min_idx;
+                _data[idx]->loc = idx;
+                perc_down(min_idx);
+                return true;
+            }
+        }
+        void perc_up(const size_type idx) {
+            if (idx == 0) { return; }
+            const size_type parent_idx = parent(idx);
+            if (_data[idx]->time < _data[parent_idx]->time) {
+                std::swap(_data[idx], _data[parent_idx]);
+                _data[idx]->loc = idx;
+                _data[parent_idx]->loc = parent_idx;
+                perc_up(parent_idx);
+            }
+        }
+        // the same functionality as heap.Fix in Golang
+        // restores heap property after ONE change of priority/time at idx
+        void fix(const size_type idx) {
+            if (!perc_down(idx)) {
+                perc_up(idx);
+            }
+        }
+
+    public:
+        EagerQueue() = default;
+
+        EagerQueue(const EagerQueue<T>& other) = delete;
+        EagerQueue(EagerQueue<T>&& other) = delete;
+        EagerQueue& operator=(const EagerQueue<T> & other) = delete;
+        EagerQueue& operator=(EagerQueue<T> && other) = delete;
+
+        ~EagerQueue() {
+            for (auto* entry: _data) {
+                delete entry;
+            }
+        }
 
         template<typename F>
-        HashQueue(F f, const std::vector<std::pair<double, T>>& data): id_func{f} {
+        explicit EagerQueue(F f): QueueBase<T, ID, Hash, KeyEqual>{f} {}
+
+        template<typename ...Arg>
+        void push(double time, Arg&&... args) {
+            auto* entry = new detail::EagerEntry<T>(_data.size(), time, std::forward<Arg>(args)...);
+            ID id = this->convert(entry->payload);
+            assert(!_m.count(id));
+            _data.push_back(entry);
+            _m.emplace(std::move(id), entry);
+            perc_up(_data.size()-1);
+        }
+
+        std::pair<double, T> pop() {
+            if (_data.empty()) {
+                throw std::runtime_error("Empty Queue");
+            }
+            auto* top = _data[0];
+            _data[0] = _data.back();
+            _data.pop_back();
+            _data[0]->loc = 0;
+            perc_down(0);
+            detail::EagerEntry<T> top_copy = std::move(*top);
+            delete top;
+            assert(_m.count(this->convert(top_copy.payload)));
+            _m.erase(this->convert(top_copy.payload));
+            return { top_copy.time, std::move(top_copy.payload) };
+        }
+
+        std::pair<double, const T&> peek() const {
+            auto* entry = _data[0];
+            return { entry->time, entry->payload };
+        }
+
+        void remove(const ID& id) {
+            auto it = _m.find(id);
+            assert(it!=_m.end());
+            auto* removed_entry = it->second;
+            _m.erase(it);
+            auto loc = removed_entry->loc;
+            delete removed_entry;
+            _data[loc] = _data.back();
+            _data.pop_back();
+            _data[loc]->loc = loc;
+            fix(loc);
+        }
+
+        void reschedule(const ID& id, double new_time) {
+            auto it = _m.find(id);
+            assert(it!=_m.end());
+            it->second->time = new_time;
+            fix(it->second->loc);
+        }
+
+        [[nodiscard]] bool contains(const ID& id) const {
+            return _m.find(id) != _m.end();
+        }
+
+        [[nodiscard]] auto size() const noexcept { return _data.size(); }
+
+    };
+}
+
+namespace data_structures {
+    template<typename T, typename ID=T, typename Hash=std::hash<ID>, typename KeyEqual=std::equal_to<ID>>
+    class LazyQueue: public QueueBase<T, ID, Hash, KeyEqual> {
+    private:
+        std::vector<detail::LazyEntry<T>*> _data;
+        using size_type = typename decltype(_data)::size_type;
+        size_type _size{};
+        std::unordered_map<ID, detail::LazyEntry<T>*, Hash, KeyEqual> _m;
+    public:
+        LazyQueue() = default;
+
+        template<typename F>
+        explicit LazyQueue(F f): QueueBase<T, ID, Hash, KeyEqual>{f} {}
+
+        template<typename F>
+        LazyQueue(F f, const std::vector<std::pair<double, T>>& data): QueueBase<T, ID, Hash, KeyEqual>{f} {
             _size = data.size();
             for (const auto& [time, payload]: data) {
-                ID id = convert(payload);
+                ID id = this->convert(payload);
                 assert(!_m.count(id));
-                auto * entry = new detail::Entry<T>(time, payload);
+                auto * entry = new detail::LazyEntry<T>(time, payload);
                 _data.push_back(entry);
                 _m.emplace(std::move(id), entry);
             }
             std::make_heap(_data.begin(), _data.end(), detail::compare<T>);
         }
 
-        explicit HashQueue(const std::vector<std::pair<double, T>>& data): HashQueue(std::nullopt, data) {}
+        explicit LazyQueue(const std::vector<std::pair<double, T>>& data): LazyQueue(std::nullopt, data) {}
 
-        ~HashQueue() {
-            for (detail::Entry<T>* e : _data) {
+        ~LazyQueue() {
+            for (detail::LazyEntry<T>* e : _data) {
                 delete e;
             }  // don't need to delete for map
         }
 
-        HashQueue(const HashQueue<T>& other) = delete;
-        HashQueue(HashQueue<T>&& other) = delete;
-        HashQueue& operator=(const HashQueue<T> & other) = delete;
-        HashQueue& operator=(HashQueue<T> && other) = delete;
+        LazyQueue(const LazyQueue<T>& other) = delete;
+        LazyQueue(LazyQueue<T>&& other) = delete;
+        LazyQueue& operator=(const LazyQueue<T> & other) = delete;
+        LazyQueue& operator=(LazyQueue<T> && other) = delete;
 
 
         template<typename ...Arg>
         void push(double time, Arg&&... args) {
-            auto* entry = new detail::Entry<T>(time, std::forward<Arg>(args)...);
-            ID id = convert(entry->payload);
+            auto* entry = new detail::LazyEntry<T>(time, std::forward<Arg>(args)...);
+            ID id = this->convert(entry->payload);
             assert(!_m.count(id));
             ++_size;
             _data.push_back(entry);
@@ -94,14 +246,14 @@ namespace data_structures {
         std::pair<double, T> pop() {
             while (_size > 0) {
                 std::pop_heap(_data.begin(), _data.end(), detail::compare<T>);
-                detail::Entry<T>* entry = _data.back();
+                detail::LazyEntry<T>* entry = _data.back();
                 _data.pop_back();
-                detail::Entry<T> entry_copy = std::move(*entry); // move the content of entry out
+                detail::LazyEntry<T> entry_copy = std::move(*entry); // move the content of entry out
                 delete entry;  // and then delete
                 if (entry_copy.exist) {
                     _size--;
-                    assert(_m.count(convert(entry_copy.payload)));
-                    _m.erase(convert(entry_copy.payload));
+                    assert(_m.count(this->convert(entry_copy.payload)));
+                    _m.erase(this->convert(entry_copy.payload));
                     return { entry_copy.time, std::move(entry_copy.payload) };
                 }
             }
@@ -110,7 +262,7 @@ namespace data_structures {
 
         std::pair<double, const T&> peek() {
             while (_size > 0) {
-                detail::Entry<T>* entry = _data[0];  // where the min time is
+                detail::LazyEntry<T>* entry = _data[0];  // where the min time is
                 if (entry->exist) {
                     return { entry->time, entry->payload };
                 }
@@ -125,7 +277,7 @@ namespace data_structures {
         void remove(const ID& id) {
             auto it = _m.find(id);
             assert(it!=_m.end() && it->second->exist);
-            detail::Entry<T>* entry = it->second;
+            detail::LazyEntry<T>* entry = it->second;
             _m.erase(it);
             entry->exist = false; // mark as deleted
             _size--;
--- tests/hash_queue_test.cpp
+++ tests/hash_queue_test.cpp
@@ -9,7 +9,7 @@
 using namespace data_structures;
 
 TEST(hq_test, test_cons) {
-    HashQueue<std::string> hEmpty;
+    LazyQueue<std::string> hEmpty;
     EXPECT_EQ(0, hEmpty.size());
     EXPECT_ANY_THROW(hEmpty.pop());
     EXPECT_ANY_THROW(hEmpty.peek());
@@ -24,7 +24,7 @@ TEST(hq_test, test_cons) {
     EXPECT_ANY_THROW(hEmpty.pop());
 
     std::vector wrong {std::make_pair(3.5, "id"), std::make_pair(6.9, "id")};
-    ASSERT_DEATH(HashQueue hWrong{wrong}, "");
+    ASSERT_DEATH(LazyQueue hWrong{wrong}, "");
 
     std::vector<std::pair<double, std::string>> v;
     std::vector<double> v1 {3, 1, 2, 5, 4};
@@ -32,7 +32,7 @@ TEST(hq_test, test_cons) {
     for (int i=0; i<v1.size(); i++) {
         v.emplace_back(v1[i], v2[i]);
     }
-    HashQueue<std::string> h(v);
+    LazyQueue<std::string> h(v);
     EXPECT_EQ(v.size(), h.size());
     h.remove("e");
     h.remove("b");
@@ -45,7 +45,8 @@ TEST(hq_test, test_cons) {
     EXPECT_EQ(v[3], h.pop());
 }
 
-TEST(hq_test, test_sort) {
+template<typename QT>
+void test_sorting() {
     std::default_random_engine generator;
     std::exponential_distribution<double> distribution(0.1);
     int num_rolls = 1000;
@@ -58,7 +59,7 @@ TEST(hq_test, test_sort) {
     // indices to remove
     std::mt19937 rng(123);
     std::uniform_int_distribution<int> gen(0, num_rolls-1);
-    int num_remove = 100;
+    int num_remove = 500;
     std::unordered_set<int> removed_indices;
     for (int i = 0; i < num_remove; ++i) {
         removed_indices.insert(i);
@@ -71,7 +72,10 @@ TEST(hq_test, test_sort) {
         }
     }
     std::sort(after_remove.begin(), after_remove.end());  // sort after removing the same indices from heap
-    HashQueue hq(nums);
+    QT hq;
+    for (auto& [n, i]: nums) {
+        hq.push(n, i);
+    }
     for (int ri : removed_indices) {
         hq.remove(ri);
     }
@@ -81,6 +85,26 @@ TEST(hq_test, test_sort) {
         EXPECT_EQ(after_remove[i], hq.pop());
         i++;
     }
+}
+
+TEST(hq_test, test_fix) {
+    EagerQueue<int> q;
+    q.push(0, 0);
+    q.push(1, 1);
+    q.push(2, 2);
+    ASSERT_EQ(q.peek().first, 0);
+    ASSERT_TRUE(q.contains(1));
+    q.reschedule(1, 666);
+    q.pop();
+    ASSERT_EQ(q.peek().first, 2);
+    q.pop();
+    ASSERT_EQ(q.peek().first, 666);
+    ASSERT_EQ(q.size(), 1);
+}
+
+TEST(hq_test, test_sort) {
+    test_sorting<LazyQueue<int>>();
+    test_sorting<EagerQueue<int>>();
 }
 
 struct A {
@@ -103,10 +127,10 @@ struct B {
 };
 
 TEST(hq_test, test_obj) {
-    HashQueue<A, std::string> cannot_convert{};
+    LazyQueue<A, std::string> cannot_convert{};
     EXPECT_DEATH(cannot_convert.push(0.0, 0, 1), "");
     auto get_id = [](const auto& p){ return p.id(); };
-    HashQueue<std::variant<A, B>, std::string> q{
+    LazyQueue<std::variant<A, B>, std::string> q{
         [&](const std::variant<A, B>& p) {
             return std::visit(get_id, p);
         }
@@ -127,7 +151,7 @@ TEST(hq_test, test_obj) {
 }
 
 TEST(hq_test, test_obj_simple) {
-    HashQueue<A, std::string> q{[](const A& a) { return a.id(); }};
+    LazyQueue<A, std::string> q{[](const A& a) { return a.id(); }};
     q.push(1.0, 0, 0);
     q.push(2.0, 0, 1);
     q.push(3.0, 1, 0);
